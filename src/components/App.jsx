05be66619d62faefd54e9a64ba1679bc75d7c92d
aijazbinqasim <aijazbinqasim@hotmail.com>
2024-08-15 20:12:20 +0500

    Improve App structure.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,42 @@
-import TodoList from './TodoList';
-import TodoCreate from './TodoCreate';
-import './App.css';
-import { useState } from 'react';
+import { useState } from "react";
+import TodoList from "./TodoList";
+import TodoCreate from "./TodoCreate";
+import "../styles/App.css";
 
+const App = () => {
+  const [todos, setTodos] = useState([]);
 
+  const createTodo = (title) => {
+    const newTodo = { id: crypto.randomUUID(), title, completed: false };
+    const updatedTodos = [...todos, newTodo];
+    setTodos(updatedTodos);
+  };
 
-const App=()=>{
+  const removeTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
 
-    const [todos,setTodos]=useState([]);
+  const changeTodo = (id, title, completed = false) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, title, completed };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
 
-    const createTodo=(title)=>{
-        const newTodo={id: crypto.randomUUID(),title,completed:false};
-        const updatedTodos=[...todos,newTodo];
-        setTodos(updatedTodos);
-    };
-
-    const removeTodo=(id)=>{
-        const updatedTodos=todos.filter((todo)=> todo.id !==id);
-        setTodos(updatedTodos);
-    };
-
-    const changeTodo=(id,title,completed=false)=>{
-       const updatedTodos=todos.map((todo)=>{
-          if(todo.id===id){
-            return {...todo,title,completed};
-          }
-          return todo;
-       });
-       setTodos(updatedTodos);
-    };
-
-    return(
-        <main className="main">
-            <div className='bg'></div>
-            <h1>
-                React Todo <span>StreamLine your Day, the react Way!</span>
-            </h1>
-            <TodoList todos={todos} removeTodo={removeTodo} changeTodo={changeTodo}/>
-            <TodoCreate createTodo={createTodo}/> 
-        </main>   
-   
-    );
+  return (
+    <main className="main">
+      <div className="bg"></div>
+      <h1>
+        React Todo <span>StreamLine your Day, the react Way!</span>
+      </h1>
+      <TodoList todos={todos} removeTodo={removeTodo} changeTodo={changeTodo} />
+      <TodoCreate createTodo={createTodo} />
+    </main>
+  );
 };
 
 export default App;
--- src/components/TodoEdit.jsx
+++ src/components/TodoEdit.jsx
@@ -1,25 +1,25 @@
 import { useState } from "react";
-import CheckIcon from '../check.svg';
+import CheckIcon from "../svgs/check.svg";
 
-const TodoEdit=({todo,onSubmit})=>{
-    const [title,setTitle]=useState(todo.title);
+const TodoEdit = ({ todo, onSubmit }) => {
+  const [title, setTitle] = useState(todo.title);
 
-    const handleChange=(e)=>{
-       setTitle(e.target.value);
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSubmit(todo.id, title);
-      };
-    
-      return (
-        <form className="todo-edit">
-          <input type="text" value={title} onChange={handleChange} />
-          <button type="submit" onClick={handleSubmit}>
-            <img src={CheckIcon} title="Save" />
-          </button>
-        </form>
-      );  
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(todo.id, title);
+  };
+
+  return (
+    <form className="todo-edit">
+      <input type="text" value={title} onChange={handleChange} />
+      <button type="submit" onClick={handleSubmit}>
+        <img alt="" src={CheckIcon} title="Save" />
+      </button>
+    </form>
+  );
 };
 
 export default TodoEdit;
